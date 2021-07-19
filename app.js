50d12d2fe03059c9204c9719a7085990861eae93
nkwsy <nick@nickwesley.me>
2021-07-19 18:55:48 -0500

    added custom ssl forwarder

--- app.js
+++ app.js
@@ -71,6 +71,17 @@ app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Custom SSL fowarder
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
+//
 app.use(expressStatusMonitor());
 app.use(compression());
 app.use(sass({
