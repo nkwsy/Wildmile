4c5aa7efead0151d34c5799e1acfed5d63145cd8
nkwsy <nick@nickwesley.me>
2023-07-09 21:28:45 -0500

    Created schema

--- models/Mod.js
+++ models/Mod.js
@@ -24,4 +24,41 @@ const ModSchema = new mongoose.Schema({
 });
 
 const Mod = mongoose.model('Mod', ModSchema);
-module.exports = Mod;
+
+const ProjectSchema = new mongoose.Schema({
+  name: String,
+  dateInstalled: Date,
+  installed: Boolean,
+  x: Number,
+  y: Number,
+  location: {type: "String", coordinates: []},
+  section: String,
+  orientation: String,
+  shape: String,
+  notes: String,
+  flag: Boolean,
+  flipped: Boolean,
+  tags: Array,
+  decommisioned: Boolean,
+  creator: { type: Schema.Types.ObjectId, ref: 'User' },
+
+},{ timestamps: true });
+
+const Project = mongoose.model('Project', ProjectSchema)
+
+const SectionSchema = new mongoose.Schema({
+  name: String,
+  dateInstalled: Date,
+  installed: Boolean,
+  x: Number,
+  y: Number,
+  location: {type: "Point", coordinates:[]},
+  project_id: {type: Schema.Types.ObjectId, ref: 'Project'}
+})
+
+const Section = mongoose.model('Section', SectionSchema)
+module.exports = {
+  Mod,
+  Project,
+  Section
+};
