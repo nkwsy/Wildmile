6b50db1587e35a857b3cfbdbb8d37bff05015a0b
nkwsy <nick@nickwesley.me>
2024-05-02 21:29:43 -0500

    Created models for cameratrap

---
+++ wildmile/app/actions/CameratrapActions.js
@@ -0,0 +1,4 @@
+import Camera from "models/cameratrap/Camera";
+import Deployment from "models/cameratrap/Deployment";
+import Media from "models/cameratrap/Media";
+import Observation from "models/cameratrap/Observation";
