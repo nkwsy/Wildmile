7b38cf10d3315616a706a1035ef42fad376b9eb3
nkwsy <nick@nickwesley.me>
2024-05-07 00:52:13 -0500

    revail again

--- wildmile/app/trashlog/[Id]/page.js
+++ wildmile/app/trashlog/[Id]/page.js
@@ -81,7 +81,7 @@ import Link from "next/link";
 // }
 
 export async function renderAccordian(logId) {
-  revalidatePath("/trashlog/[Id]");
+  revalidatePath("/trashlog");
   const props = await getItemsFromLog(logId);
   console.log("Props:", props);
   return <TrashItemAccordian props={props} />;
