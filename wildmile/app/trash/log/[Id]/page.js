b5fef3c3674ea4dac5d0fa83f37a41c60141c0a9
nkwsy <nick@nickwesley.me>
2024-05-08 00:43:59 -0500

    got rid of old history page

--- wildmile/app/trash/log/[Id]/page.js
+++ wildmile/app/trash/log/[Id]/page.js
@@ -22,66 +22,8 @@ import { getItemsFromLog } from "app/actions/TrashActions";
 import { revalidatePath } from "next/cache";
 
 import Link from "next/link";
-// import { use } from "passport";
-
-// export async function getProps(logId) {
-//   //   await dbConnect();
-//   // const logId = context.params.id
-//   const id = logId; // Get the id from the request parameters
-//   console.log("id" + id);
-//   /* find all the data in our database */
-//   const result = await TrashItem.find({ deleted: false }, [
-//     "-creator",
-//     "-__v",
-//     "-createdAt",
-//     "-updatedAt",
-//     "-deleted",
-//   ])
-//     .populate({
-//       path: "individualTrashItem",
-//       // match: { logId: mongoose.Types.ObjectId(id) },
-//       match: { logId: id },
-//       model: "IndividualTrashItem",
-//       select: "-__v -createdAt -updatedAt -deleted -creator",
-//     })
-//     .lean();
-
-//   const items = result.reduce((acc, doc) => {
-//     const item = doc; //.toObject()
-//     item._id = item._id.toString();
-
-//     // If an IndividualTrashItem was found, add its quantity to the item
-//     if (!item.individualTrashItem) {
-//       item.quantity = 0;
-//       console.log("no individualTrashItem");
-//     } else {
-//       // item.individualTrashItem._id = item.individualTrashItem._id.toString();
-//       item.individualTrashItem._id = item.individualTrashItem._id.toString();
-//       item.individualTrashItem.itemId =
-//         item.individualTrashItem.itemId.toString();
-//       item.individualTrashItem.logId =
-//         item.individualTrashItem.logId.toString();
-//       item.quantity = item.individualTrashItem.quantity;
-//       // item.weight = item.individualTrashItem.weight; // Add this line if you also want to include weight
-//     }
-//     // else {
-//     //   item.quantity = 0; // Default quantity
-//     //   // item.weight = 0; // Default weight
-//     // }
-
-//     // Remove the individualTrashItem property from the item
-//     delete item.individualTrashItem;
-//     // Use the _id of the item as the key
-//     acc[item._id] = item;
-//     return acc;
-//   }, {});
-
-//   console.log(items);
-//   return { items: items, logId: id };
-// }
 
 export async function renderAccordian(logId) {
-  // revalidatePath("/trashlog");
   const props = await getItemsFromLog(logId);
   console.log("Props:", props);
   return <TrashItemAccordian props={props} />;
