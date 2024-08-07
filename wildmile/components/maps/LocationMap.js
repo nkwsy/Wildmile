c526c7ddfc72303cc3bfc809ed6ae5724e56215e
Xinjiang Shao <shaoxinjiang@gmail.com>
2024-08-06 22:30:15 -0500

    feat: add browser geolocation for current location fetching and adjust map layout
    Ref #194

--- wildmile/components/maps/LocationMap.js
+++ wildmile/components/maps/LocationMap.js
@@ -1,13 +1,13 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import classes from "styles/map.module.css";
 
+import styles from "./LocationMap.module.css";
 // import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 // mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-function updateArea(e) {}
 
 const pointStyle = [
   {
@@ -43,9 +43,10 @@ const pointStyle = [
 const LocationMap = ({ onPointSelect, onPolygonSelect, existingLocations }) => {
   mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY;
   const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-87.65);
-  const [lat, setLat] = useState(41.9);
+  // default center to be wild mile location
+  const [lng, setLng] = useState(-87.6521);
+  const [lat, setLat] = useState(41.9068);
+  const [zoom, setZoom] = useState(17);
   const [marker, setMarker] = useState(null);
   const [point, setPoint] = useState(null);
   const [polygon, setPolygon] = useState(null);
@@ -72,7 +73,6 @@ const LocationMap = ({ onPointSelect, onPolygonSelect, existingLocations }) => {
   if (typeof onPointSelect === "function") {
     usePoints = true;
   }
-
   // Check if onPolygonSelect is a function
   if (typeof onPolygonSelect === "function") {
     usePolygons = true;
@@ -80,19 +80,9 @@ const LocationMap = ({ onPointSelect, onPolygonSelect, existingLocations }) => {
   let Draw = new MapboxDraw({
     controls: { point: usePoints, polygon: usePolygons, trash: true },
     displayControlsDefault: false,
-    // styles: pointStyle,
+    styles: pointStyle,
   });
-  useEffect(() => {
-    if (map.current) return; // Initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: 9,
-    });
-
-    map.current.addControl(Draw, "top-left");
-    map.current.on("draw.create", function (e) {
+  const updateArea = function (e) {
       console.log(e.features);
       console.log(Draw.getAll());
       if (e.features[0].geometry.type === "Polygon") {
@@ -113,16 +103,44 @@ const LocationMap = ({ onPointSelect, onPolygonSelect, existingLocations }) => {
         if (feature.geometry.type === "Polygon" && feature.id !== newId) {
           Draw.delete(feature.id);
         }
-      });
-    });
-  }, []);
+      })
+  };
 
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.addControl(Draw, "top-left");
+    map.on("draw.create", updateArea);
+    map.on("draw.delete", updateArea);
+    map.on("draw.update", updateArea);
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+    return () => map.remove();
+  }, []);
+  
   return (
-    <div
-      ref={mapContainer}
-      className={classes.map}
-      // style={{ height: "400px", width: "100%" }}
-    />
+    <>
+      <div className={styles.container}>
+        <div className={styles.sidebarStyle}>
+          <div>
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+          </div>
+        </div>
+        <div
+          ref={mapContainer}
+          className={classes.map}
+          // style={{ height: "400px", width: "100%" }}
+        />
+      </div>
+    </>
   );
 };
 
