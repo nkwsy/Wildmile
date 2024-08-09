6de8ed3148d1afeef8449d27add53ffb6bdb203b
nkwsy <nick@nickwesley.me>
2024-08-08 19:11:01 -0500

    Got rid of plant tag route

--- wildmile/app/api/plants/getTags/route.js
+++ wildmile/app/api/plants/getTags/route.js
@@ -1,11 +1,11 @@
-import { getPlantTags } from "/actions/PlantActions";
+// import { getPlantTags } from "/actions/PlantActions";
 
-export async function GET(request, { params, query, res }) {
-  try {
-    const result = await getPlantTags();
-    return Response.json(JSON.stringify(result));
-  } catch (error) {
-    console.error(error);
-    return { success: false, error: "Internal Server Error" };
-  }
-}
+// export async function GET(request, { params, query, res }) {
+//   try {
+//     const result = await getPlantTags();
+//     return Response.json(JSON.stringify(result));
+//   } catch (error) {
+//     console.error(error);
+//     return { success: false, error: "Internal Server Error" };
+//   }
+// }
