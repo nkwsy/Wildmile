96c741789211be89a89571cb37a6b29370b7029a
nick <nick@urbanriv.org>
2024-11-06 00:04:28 -0600

    added filter for animals

--- wildmile/app/api/cameratrap/deployments/[id]/images/route.js
+++ wildmile/app/api/cameratrap/deployments/[id]/images/route.js
@@ -11,10 +11,41 @@ export async function GET(request, { params }) {
   const { searchParams } = new URL(request.url);
   const page = parseInt(searchParams.get("page")) || 1;
   const limit = parseInt(searchParams.get("limit")) || 12;
+  const type = searchParams.get("type");
+  const date = searchParams.get("date");
+  const time = searchParams.get("time");
   const skip = (page - 1) * limit;
 
   try {
     const query = { deploymentId: params.id };
+
+    if (type === 'animals') {
+      query['speciesConsensus'] = {
+        $elemMatch: { observationType: 'animal' }
+      };
+    } else if (type === 'humans') {
+      query['speciesConsensus'] = {
+        $elemMatch: { observationType: 'human' }
+      };
+    }
+
+    if (date || time) {
+      const timestamp = {};
+      if (date) {
+        const dateObj = new Date(date);
+        timestamp.$gte = new Date(dateObj.setHours(0, 0, 0, 0));
+        timestamp.$lt = new Date(dateObj.setHours(23, 59, 59, 999));
+      }
+      if (time) {
+        const [hours, minutes] = time.split(':');
+        const timeDate = new Date();
+        timeDate.setHours(parseInt(hours), parseInt(minutes), 0, 0);
+        timestamp.$gte = timeDate;
+        timeDate.setMinutes(timeDate.getMinutes() + 1);
+        timestamp.$lt = timeDate;
+      }
+      query.timestamp = timestamp;
+    }
 
     const [images, totalImages] = await Promise.all([
       CameratrapMedia.find(query)
