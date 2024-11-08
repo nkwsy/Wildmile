35a9a875d62b7e418b53875e2f920936042d9d71
nkwsy <nick@nickwesley.me>
2024-11-08 13:01:32 -0600

    Added new personalized searches

--- wildmile/components/cameratrap/images/GalleryFilter.js
+++ wildmile/components/cameratrap/images/GalleryFilter.js
@@ -1,15 +1,21 @@
 "use client";
 import { AdvancedImageFilterControls } from "../AdvancedImageFilterControls";
 import { Button, Group, Drawer, Stack, Collapse } from "@mantine/core";
-import { IconRefresh, IconAdjustmentsHorizontal } from "@tabler/icons-react";
-import { useState } from "react";
+import {
+  IconRefresh,
+  IconAdjustmentsHorizontal,
+  IconPaw,
+} from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 import { useDisclosure } from "@mantine/hooks";
 import WildlifeSearch from "../WildlifeSearch";
 import { useSelection } from "../ContextCamera";
 
 export function GalleryFilter({ onFilterChange }) {
-  const [opened, { toggle }] = useDisclosure(false);
+  const [opened, { toggle }] = useDisclosure(true);
   const [selection, setSelection] = useSelection();
+  const [minimized, { toggle: toggleMinimized }] = useDisclosure(true);
+
   const handleApplyFilters = (filters) => {
     // if (onFilterChange) {
     if (selection.length > 0) {
@@ -32,6 +38,16 @@ export function GalleryFilter({ onFilterChange }) {
     // }
   };
 
+  const handleClearFilters = () => {
+    setSelection([]);
+  };
+  // useEffect(() => {
+  //   if (opened) {
+  //     setMinimized(false);
+  //   } else {
+  //     setMinimized(true);
+  //   }
+  // }, [opened]);
   return (
     <>
       {/* <Group position="apart">
@@ -51,11 +67,31 @@ export function GalleryFilter({ onFilterChange }) {
           {hasActiveFilters ? "Filters Active" : "Filters"}
         </Button>
       </Group> */}
+      <Group justify="flex-start" mb={5}>
+        <Button
+          onClick={toggle}
+          variant="subtle"
+          color="yellow"
+          leftSection={<IconPaw size={16} />}
+        >
+          {opened ? "Hide Wildlife" : "Show Wildlife"}
+        </Button>
 
-      <AdvancedImageFilterControls onApplyFilters={handleApplyFilters} />
-      <Group justify="center" mb={5}>
-        <Button onClick={toggle}>Filter by Wildlife</Button>
+        <Button
+          onClick={toggleMinimized}
+          variant="outline"
+          color="yellow"
+          leftSection={<IconAdjustmentsHorizontal size={16} />}
+        >
+          {minimized ? "Hide Filters" : "Show Filters"}
+        </Button>
       </Group>
+      <AdvancedImageFilterControls
+        onApplyFilters={handleApplyFilters}
+        onClearFilters={handleClearFilters}
+        onMinimize={minimized}
+      />
+
       <Collapse in={opened}>
         <WildlifeSearch />
       </Collapse>
