4db4756d3406707707370f3122db31c7238b96fd
nkwsy <nick@nickwesley.me>
2025-02-04 15:48:36 -0600

    Changed up mongoose dbConnect

--- wildmile/lib/db/setup.js
+++ wildmile/lib/db/setup.js
@@ -1,13 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -20,6 +12,7 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  const MONGODB_URI = process.env.MONGODB_URI;
   if (cached.conn) {
     return cached.conn;
   }
