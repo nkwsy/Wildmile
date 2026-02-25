da1661e0cf6880a1c4e79d98950b714ab3d0004d
nkwsy <nick@nickwesley.me>
2026-02-24 18:32:09 -0600

    got rid of cache for random image

--- wildmile/app/api/cameratrap/randomFavorite/route.js
+++ wildmile/app/api/cameratrap/randomFavorite/route.js
@@ -30,7 +30,7 @@ export async function GET() {
 
     return NextResponse.json(randomFavorite[0], {
       headers: {
-        "Cache-Control": "public, s-maxage=60, stale-while-revalidate=120",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
       },
     });
   } catch (error) {
